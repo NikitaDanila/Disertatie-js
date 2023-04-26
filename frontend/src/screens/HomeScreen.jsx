c2eede6c120077972540ae53699ba8dc2da64b33
Nikita Danila <danila.nikitamihai@gmail.com>
2023-04-26 21:43:34 +0300

    redirect to register if not logged in homepage

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,12 +2,21 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Profile from "../components/Profile";
 
 function HomeScreen() {
+  const navigateTo = useNavigate();
   const [profiles, setProfiles] = useState([]);
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
+
   useEffect(() => {
+    if (!userInfo) {
+      navigateTo("/register");
+    }
     async function fetchProfiles() {
       const { data } = await axios.get("/api/users/getProfiles");
       // console.log(data);
